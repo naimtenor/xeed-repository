868e756170a942cb101176fe9a31eef8b4f0155a
naimtenor <naimtenor@gmail.com>
2016-12-11 21:53:38 +0900

    initail commit

---
+++ cjz-se/src/main/java/sally/se/CjzSeApplication.java
@@ -0,0 +1,12 @@
+package sally.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjzSeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CjzSeApplication.class, args);
+	}
+}
